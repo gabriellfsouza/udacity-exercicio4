827a2fb65b34534090742f9e86f893ce71252f72
root <gabriellfsouza@gmail.com>
2019-02-03 23:40:27 +0000

    final

--- src/App.js
+++ src/App.js
@@ -5,12 +5,35 @@ import './App.css';
 const value1 = Math.floor(Math.random() * 100);
 const value2 = Math.floor(Math.random() * 100);
 const value3 = Math.floor(Math.random() * 100);
-const proposedAnswer = Math.floor(Math.random() * 3) + value1 + value2 + value3;
-const numQuestions = 0;
-const numCorrect = 0;
 
 class App extends Component {
+  state = {
+  	value1 : value1,
+    value2 : value2,
+    value3 : value3,
+    proposedAnswer : Math.floor(Math.random() * 3) + value1 + value2 + value3,
+    numQuestions : 0,
+    numCorrect : 0
+  }
+
+  verifyResult = (resposta) => {
+    let value1 = Math.floor(Math.random() * 100),
+      	value2 = Math.floor(Math.random() * 100),
+      	value3 = Math.floor(Math.random() * 100),
+        proposedAnswer = Math.floor(Math.random() * 3) + value1 + value2 + value3;
+    //debugger;
+    this.setState(prevState=>({
+    	numQuestions : prevState.numQuestions + 1,
+      	numCorrect : prevState.numCorrect + (((prevState.value1 + prevState.value2 + prevState.value3 == prevState.proposedAnswer) == resposta)? 1 : 0),
+      	value1 : value1,
+      	value2 : value2,
+      	value3 : value3,
+      	proposedAnswer : proposedAnswer
+    }));
+  };
+
   render() {
+    
     return (
       <div className="App">
         <header className="App-header">
@@ -20,12 +43,12 @@ class App extends Component {
         <div className="game">
           <h2>Mental Math</h2>
           <div className="equation">
-            <p className="text">{`${value1} + ${value2} + ${value3} = ${proposedAnswer}`}</p>
+            <p className="text">{`${this.state.value1} + ${this.state.value2} + ${this.state.value3} = ${this.state.proposedAnswer}`}</p>
           </div>
-          <button>True</button>
-          <button>False</button>
+          <button onClick={()=>this.verifyResult(true)} >True</button>
+          <button onClick={()=>this.verifyResult(false)}>False</button>
           <p className="text">
-            Your Score: {numCorrect}/{numQuestions}
+            Your Score: {this.state.numCorrect}/{this.state.numQuestions}
           </p>
         </div>
       </div>
